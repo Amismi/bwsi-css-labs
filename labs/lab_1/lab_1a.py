d51caecf9a30559928d4a486ef7bc180deac0b59
Amismi <149420372+Amismi@users.noreply.github.com>
2026-03-28 19:03:35 -0700

    Change Middle name
    Added middle name to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,7 +8,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "Ismael Shuja"
+    name = "Ismael Amir Shuja"
 
     print(f"{name}, Welcome to the CSS course!")
 
